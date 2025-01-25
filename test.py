4f36962c27f5edf25d069c37a57865f001012848
DerrickLapp <Derrick.Lapp@sdgku.edu>
2025-01-25 11:43:56 -0800

    Assignment 2 example counters

--- test.py
+++ test.py
@@ -55,3 +55,54 @@ me["last_name"] = "NewLapp"
 print(me)
 me["color"] = "Yellow"
 print(me)
+
+
+ages = [32, 74, 20, 69, 52, 26, 31, 77, 43, 73, 51, 57, 19, 79, 40, 34, 27, 23, 21, 44, 53, 55, 24, 36, 41, 47, 78, 46, 68, 75, 49, 83, 61, 60, 29, 56, 67, 17, 70, 81, 87, 38]
+
+# print the sum of all the numbers
+def ex1():
+    total = 0
+    for age in ages:
+        total = total + age
+    print(total)
+# Count how many users are equal or older than 21
+# My version
+def ex2():
+    for adults in ages:
+        if adults >= 21:
+            adults += 1
+    print(adults)
+# Adrian's version
+def Adex2():
+    counter = 0
+    for age in ages:
+        if age >= 21:
+            counter += 1
+    print(counter)
+# How many users are between 30 and 40 years old
+# My Version
+def ex3():
+    counter = 0
+    for age in ages:
+        if age >= 30:
+            if age <= 40:
+                counter += 1
+            else:
+                counter += 0
+        else:
+            counter += 0
+    print(counter)
+# Adrian Version
+def Adex3():
+    counter = 0
+    for age in ages:
+        if age >= 30 and age <= 40:
+            counter += 1
+    print("There are " + str(counter) + " users between 30 and 40.")
+
+# Call the functions
+ex1()
+ex2()
+Adex2()
+ex3()
+Adex3()
